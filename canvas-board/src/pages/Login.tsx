6ef71ff3d388f87bc9871d2cebc43ce08ef444c4
rohit20001221 <rohit20001221@gmail.com>
2020-09-12 11:36:37 +0530

    frontend, fixed auth screen css issues

--- canvas-board/src/pages/Login.tsx
+++ canvas-board/src/pages/Login.tsx
@@ -1,39 +1,26 @@
-import React from 'react';
-import "./App.css";
+import React from "react";
+import "./Login.css";
+import logo from "./logo.png";
+import { IonButton, IonText } from "@ionic/react";
+import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
-
-    return (
-        <div className="container">
-        <div className="img-container">
-            <div className="text-center">
-                <img src="logo.png" class="rounded" alt="Logo"/>
-            </div>
-        </div>
-        <div className="txt-container">
-          <h1 className="display-4">Hello there, Welcome Back!</h1>
-        </div>
-    </div>
-    <div className="container">
-      <form className="register-form">
-        <div className="row">
-          <div className="form-group col-lg-5 col-sm-5 col-md-6">
-            <label className="form-text"for="userName"> Username:</label>
-            <input type="userName" class="form-control" id="userName" placeholder="Create your username" name="userName">
-          </div>
-        </div>
-        <div className="row">
-          <div className="form-group col-lg-5 col-sm-5 col-md-6">
-            <label className="form-text"for="passwrd">Password:</label>
-            <input type="password" class="form-control" id="password" placeholder="Create password" name="password">
-          </div>
-        </div>
-        <div className="submit-button">
-            <button type="button" className="btn btn-primary btn-lg">Log In</button>
-        </div>
+  return (
+    <div className="login">
+      <img src={logo} />
+      <IonText className="welcome__text">Welcome back to Web Board</IonText>
+      <form className="login__form">
+        <IonText>Email</IonText>
+        <input className="login__formInput" type="text" />
+        <IonText>Password</IonText>
+        <input className="login__formInput" type="text" />
+        <IonButton className="login__submit">login</IonButton>
       </form>
-      <h4 className="member-login">New here? <u><a href="index.html" className="login-link">Sign Up Instead</a></u> </h4>
-    );
+      <Link to="/register">
+        <small>dont have account ?</small>
+      </Link>
+    </div>
+  );
 };
 
 export default Login;
--- canvas-board/src/pages/Register.tsx
+++ canvas-board/src/pages/Register.tsx
@@ -1,47 +1,28 @@
-import React from 'react';
-import "../App.css"
+import React from "react";
+import "./Register.css";
+import logo from "./logo.png";
+import { IonButton, IonText } from "@ionic/react";
+import { Link } from "react-router-dom";
 
 const Register: React.FC = () => {
-
-    return(
-        <div classNameNameName="container">
-        <div classNameNameName="img-container">
-            <div classNameNameName="text-center">
-                <img src="logo.png" classNameNameName="rounded" alt="Logo"/>
-            </div>
-        </div>
-        <div classNameNameName="txt-container">
-          <h1 classNameNameName="display-4"> Get on Canvas Web Board!</h1>
-        </div>
+  return (
+    <div className="register">
+      <img src={logo} />
+      <IonText className="welcome__text">Welcome to Web Board</IonText>
+      <form className="register__form">
+        <IonText>Username</IonText>
+        <input className="register__formInput" type="text" />
+        <IonText>Email</IonText>
+        <input className="register__formInput" type="text" />
+        <IonText>Password</IonText>
+        <input className="register__formInput" type="text" />
+        <IonButton className="register__submit">register</IonButton>
+      </form>
+      <Link to="/login">
+        <small>already a member ?</small>
+      </Link>
     </div>
-    <div classNameNameName="container">
-        <form classNameNameName="register-form">
-          <div classNameName="row">
-            <div classNameName="form-group col-lg-5 col-sm-5 col-md-6">
-              <label classNameName="form-text"for="userName"> Username:</label>
-              <input type="userName" classNameName="form-control" id="userName" placeholder="Create your username" name="userName">
-            </div>
-          </div>
-          <div classNameName="row">
-            <div classNameName="form-group col-lg-5 col-sm-5 col-md-6">
-              <label classNameName="form-text"for="email">Email:</label>
-              <input type="email" classNameName="form-control" id="email" placeholder="Enter email" name="email">
-            </div>
-          </div>
-          <div classNameName="row">
-            <div classNameName="form-group col-lg-5 col-sm-5 col-md-6">
-              <label classNameName="form-text"for="passwrd">Password:</label>
-              <input type="password" classNameName="form-control" id="password" placeholder="Create password" name="password">
-            </div>
-          </div>
-          <div classNameName="submit-button">
-              <button type="button" classNameName="btn btn-primary btn-lg">Sign Up</button>
-          </div>
-        </form>
-      <h4 classNameName="member-login">I am already a member <u><a href="login.html" classNameName="login-link">Login</a></u> </h4>
-    </div>
-
-    );
+  );
 };
 
 export default Register;
